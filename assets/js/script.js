3c67c180d5d55c098922ddcf78586372ca91021a
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-02-18 11:12:23 -0300

    atualizando o js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const letraMaiuscula = document.getElementById("upperCase")
+
+const caixaDigitacao = document.getElementById("caixa-input")
+
+const caixa = document.getElementById("caixa")
+
+
+function functionUpper() {
+  
+
+    // console.log(`O texto está: ${caixa.toUpperCase()}`)
+
+    caixa.innerText= `${caixaDigitacao.toUpperCase()}`
+
+
+
+
+
+}
